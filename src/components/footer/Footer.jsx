0d39eb3b6b9af8fea320cd39cd8021520cf66510
TheCatalystAdmin <nehjoshi5@gmail.com>
2023-07-02 10:34:23 +0100

    Started styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,39 @@
+import Image from 'next/image';
+import styles from './footer.module.css';
 const Footer = () => {
   return (
-    <div>
-        <div>@2023 Lamamia. All rights reserved.</div>
-        <div></div>
+    <div className={styles.container}>
+      <div>@2023 Lamamia. All rights reserved.</div>
+      <div className={styles.social}>
+          <Image
+            src="/1.png"
+            alt="Lamadev FB"
+            width={15}
+            height={15}
+            className={styles.icon}
+          />
+          <Image
+            src="/2.png"
+            alt="Lamadev IG"
+            width={15}
+            height={15}
+            className={styles.icon}
+          />
+          <Image
+            src="/3.png"
+            alt="Lamadev Twitter"
+            width={15}
+            height={15}
+            className={styles.icon}
+          />
+          <Image
+            src="/4.png"
+            alt="Lamadev YT"
+            width={15}
+            height={15}
+            className={styles.icon}
+          />
+      </div>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,50 +1,55 @@
 import Link from 'next/link';
-import React from 'react'
+import styles from './navbar.module.css';
 
 const links = [
-    {
-      id: 1,
-      title: "Home",
-      url: "/",
-    },
-    {
-      id: 2,
-      title: "Portfolio",
-      url: "/portfolio",
-    },
-    {
-      id: 3,
-      title: "Blog",
-      url: "/blog",
-    },
-    {
-      id: 4,
-      title: "About",
-      url: "/about",
-    },
-    {
-      id: 5,
-      title: "Contact",
-      url: "/contact",
-    },
-    {
-      id: 6,
-      title: "Dashboard",
-      url: "/dashboard",
-    },
-  ];
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 3,
+    title: "Blog",
+    url: "/blog",
+  },
+  {
+    id: 4,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
 
 const Navbar = () => {
-    return (
-        <div>
-            <Link href='/'>lamamia</Link>
-            <div>
-                {links.map(link => (
-                    <Link key={link.id} href={link.url}>{link.title}</Link>
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <Link className={styles.logo} href='/'>lamamia</Link>
+      <div className={styles.links}>
+        {links.map(link => (
+          <Link
+            className={styles.link}
+            key={link.id}
+            href={link.url}
+          >{link.title}</Link>
+        ))}
+        <button className={styles.logout}>Logout</button>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar;
